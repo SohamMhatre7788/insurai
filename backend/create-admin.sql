89fb045ee0a6a65099b822944094db22e5e5ba6b
Soham Mhatre <Sohammhatre7788@gmail.com>
2026-01-17 20:23:30 +0530

    some changes in manage client and manage policy page

--- backend/create-admin.sql
+++ backend/create-admin.sql
@@ -5,3 +5,8 @@ INSERT INTO users (first_name, last_name, email, password_hash, role, created_at
 VALUES ('Admin', 'User', 'admin@insurai.com', 
 '$2a$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewY5GykIJZLIZJui', 
 'ADMIN', NOW(), NOW());
+-- To update in db... 
+use Insurai_db;
+UPDATE users 
+SET role = 'ADMIN'
+WHERE id=123;
